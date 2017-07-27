a76cc0b72923edb631cf725d6d3d01779c91bd8e
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-07-27 16:47:07 -0400

    first commit

---
+++ Assets/block-breaker-assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Level load requested for: "+name);
+		Application.LoadLevel(name);
+	}
+	public void QuitRequested(string name){
+		Debug.Log("Quit requested for: "+name);
+		Application.Quit();
+	}
+}
---
+++ Assets/block-breaker-assets/Sounds/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+	
+	void Awake(){
+		Debug.Log("Music player awake "+ GetInstanceID());
+	}
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Music player start "+ GetInstanceID());
+		if( instance != null){
+			Destroy(gameObject);
+			print("MP deleted");
+		} else{
+			instance = this;
+		}
+		GameObject.DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
