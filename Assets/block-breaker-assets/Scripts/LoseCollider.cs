4d85f527c258db5a8c8418ca2bf8699f3256c38e
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-07-28 16:14:16 -0400

    attempting to trigger win screen upon collision

--- Assets/block-breaker-assets/Scripts/LoseCollider.cs
+++ Assets/block-breaker-assets/Scripts/LoseCollider.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class LoseCollider : MonoBehaviour {
 
+	public LevelManager levelManager;
+
 	// Use this for initialization
 	void OnTriggerEnter2D(Collider2D trigger){
 		print("Trigger");
 	}
 	void OnCollisionEnter2D(Collision2D collision){
 		print("Collision");
+		levelManager.LoadLevel("Win Screen");
 	}
+
 	// Update is called once per frame
 	void Update () {
 		
