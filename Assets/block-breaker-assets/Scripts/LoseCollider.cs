c62389b9d5c5f37f760ddc92374176e6234b266b
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-07-28 15:16:42 -0400

    Added LoseCollider script: OnCollisionEnter2D & OnTriggerEnter2D methods

---
+++ Assets/block-breaker-assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D trigger){
+		print("Trigger");
+	}
+	void OnCollisionEnter2D(Collision2D collision){
+		print("Collision");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
