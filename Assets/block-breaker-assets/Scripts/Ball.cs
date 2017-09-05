c1fdbe0c9878f3bd98dc3decbbbe35cce226b589
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-09-05 05:30:20 -0400

    ball bounces & game begins off click detection

--- Assets/block-breaker-assets/Scripts/Ball.cs
+++ Assets/block-breaker-assets/Scripts/Ball.cs
@@ -6,6 +6,7 @@ public class Ball : MonoBehaviour {
 
 	public Paddle paddle;
 	private Vector3 paddleToBallVector;
+	private bool hasStarted = false;
 	// Use this for initialization
 	void Start () {
 		paddleToBallVector = this.transform.position - paddle.transform.position;
@@ -13,9 +14,13 @@ public class Ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = paddle.transform.position + paddleToBallVector;
-		if(Input.GetMouseButtonDown(0)){
-			print("mouse clicked");
+		if(!hasStarted){
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			if(Input.GetMouseButtonDown(0)){
+				print("mouse clicked");
+				hasStarted = true;
+				this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 10f);
+			}
 		}
 	}
 }
