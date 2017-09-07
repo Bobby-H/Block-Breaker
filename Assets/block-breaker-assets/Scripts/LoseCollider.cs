6976b0f515783da2415a699f5877c662f9e6e28a
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-09-07 04:22:12 -0400

    made lvl_03 to test level production, prefabs working

--- Assets/block-breaker-assets/Scripts/LoseCollider.cs
+++ Assets/block-breaker-assets/Scripts/LoseCollider.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class LoseCollider : MonoBehaviour {
 
-	public LevelManager levelManager;
+	private LevelManager levelManager;
 
 	// Use this for initialization
 	void OnTriggerEnter2D(Collider2D trigger){
 		print("Trigger");
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
