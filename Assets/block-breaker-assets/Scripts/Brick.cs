94aa82696166ed9107b70c40e2a43d9fcc20d62f
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-09-23 05:29:00 -0400

    bricks now simualting win

--- Assets/block-breaker-assets/Scripts/Brick.cs
+++ Assets/block-breaker-assets/Scripts/Brick.cs
@@ -11,6 +11,7 @@ public class Brick : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
 	
 	// Update is called once per frame
